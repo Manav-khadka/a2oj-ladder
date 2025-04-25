9d7e1dd168f452dd327e6dca7030cfa17964bfaf
manav <manavkhadka2004@gmail.com>
2025-04-25 22:11:09 +0530

    Add main method placeholder in template.java

--- template.java
+++ template.java
@@ -1,6 +1,11 @@
 import java.io.*;
 import java.util.*;
 public class Main{
+    public static void main(String[] args){
+        
+    }
+    
+    
     static class FastScanner {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer("");
